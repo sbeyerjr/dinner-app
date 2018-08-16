05fc5e4366c50711859e6ba26e5d2f9cee44e538
Steven Beyer <sbeyerjr@gmail.com>
2018-08-16 16:31:33 -0700

    massive changes to javascript and css

--- main.js
+++ main.js
@@ -22,33 +22,113 @@ function getDataFromApi(lat, lon, callback) {
 }
 
 
-
 function displayGoogleLatitude(data) {
   const latitude = data.results[0].geometry.location.lat;
   const longitude = data.results[0].geometry.location.lng;
-  
-  // const results = data.results[0].map((item, index) => renderResult(item));
   getDataFromApi(latitude, longitude, displayZomatoData);
-
 }
 
+
 function displayZomatoData(data) {
-	
 	const results = data.restaurants;
 	renderResult(data);
-	console.log(data);
-	
 }
 
 
 function renderResult(result) {
+	const outputElem = $('.js-search-results');
 	const rand = Math.floor(Math.random() * 19);
 	const rest = result.restaurants[rand];
-	const text = `<h2>${rest.restaurant.name}</h2><br>
-	<img src="${rest.restaurant.thumb}">
-	<p>${rest.restaurant.location.address}</p>`;
-	$('.js-search-results').html(text);
-	
+	function priceRange () {
+	if (rest.restaurant.price_range == 1){
+
+		const price_1 = `<p>Price: <i class="fas fa-money-bill-alt"></i></p>`;
+		return price_1;
+
+	}
+	else if (rest.restaurant.price_range == 2){
+		const price_2 = `<p>Price: <i class="fas fa-money-bill-alt"></i> <i class="fas fa-money-bill-alt"></i></p>`;
+		return price_2;
+	}
+	else if (rest.restaurant.price_range == 3){
+		const price_3 = `<p>Price: <i class="fas fa-money-bill-alt"></i> <i class="fas fa-money-bill-alt"></i> <i class="fas fa-money-bill-alt"></i></p>`;
+		return price_3;
+	}
+	else {
+		return  `Price: <i class="fas fa-money-bill-alt"></i> <i class="fas fa-money-bill-alt"></i> <i class="fas fa-money-bill-alt"></i> <i class="fas fa-money-bill-alt"></i></i>`;
+	}
+
+}
+	const text = `
+	<div class="row top-results">
+	<div class="col-2">
+	<img src="${rest.restaurant.thumb}" class="thumb-img">
+	</div>
+	<div class="col-6">
+	<h2 class="restaurant-name">${rest.restaurant.name}</h2><br>
+	<h3 class="city-name">${rest.restaurant.location.city}</h3>
+	</div>
+	<div class="col-3">
+	<h3 class="user-rating">User Rating: <br>${rest.restaurant.user_rating.rating_text}</h3>
+	</div>
+	</div>
+	<div class="row">
+	<div class="col-6">
+	<p>Address: ${rest.restaurant.location.address}</p>
+	<p class="price-range">${priceRange()}</p>
+	<p>Average Cost for Two: $${rest.restaurant.average_cost_for_two}</p>
+	</div>
+	<div class="col-6">
+	<form action="http://www.google.com/maps/place/${rest.restaurant.location.latitude},${rest.restaurant.location.longitude}" target="_blank">
+	<button type="submit" class="result-btn">Directions <i class="fas fa-map-marked-alt"></i> </button><br>
+	</form>
+	<form action="${rest.restaurant.menu_url}" target="_blank" >
+	<button type="submit" class="result-btn">Menu <i class="fas fa-utensils"></i></button>
+	</form>
+	</div>
+	</div>`;
+
+	const text_noimage = `
+	<div class="row">
+	<div class="col-2">
+	<img src="https://cdn.pixabay.com/photo/2014/09/17/20/26/restaurant-449952_1280.jpg" class="thumb-img">
+	</div>
+	<div class="col-6">
+	<h2 class="restaurant-name">${rest.restaurant.name}</h2><br>
+	<h3 class="city-name">${rest.restaurant.location.city}</h3>
+	</div>
+	<div class="col-3">
+	<h3 class="user-rating">User Rating: <br>${rest.restaurant.user_rating.rating_text}</h3>
+	</div>
+	</div>
+	<div class="row">
+	<div class="col-6">
+	<p>Address: ${rest.restaurant.location.address}</p>
+	<p style="price-range">${priceRange()}</p>
+	<p>Average Cost for Two: $${rest.restaurant.average_cost_for_two}</p>
+	</div>
+	<div class="col-6">
+	<form action="http://www.google.com/maps/place/${rest.restaurant.location.latitude},${rest.restaurant.location.longitude}" target="_blank">
+	<button type="submit" class="result-btn">Directions <i class="fas fa-map-marked-alt"></i> </button><br>
+	</form>
+	<form action="${rest.restaurant.menu_url}" target="_blank" >
+	<button type="submit" class="result-btn">Menu <i class="fas fa-utensils"></i></button>
+	</form>
+	</div>
+	</div>
+	`;
+
+	if (rest.restaurant.thumb == ""){
+		$('.js-search-results').html(text_noimage);
+	}
+	else {
+		$('.js-search-results').html(text);
+	}
+	console.log(`${priceRange()}`);
+	$('.js-search-results').removeClass('hidden');
+	$('.search-box').addClass('hidden');
+	$('.box').addClass('hidden');
+	$('.top-search-box').removeClass('hidden');
 }
 
 
@@ -57,6 +137,10 @@ function watchSubmit() {
     event.preventDefault();
     const queryTarget = $(event.currentTarget).find('#js-query');
     const query = queryTarget.val();
+    const queryRadius = $(event.currentTarget).find('#radius');
+    const queryR = queryRadius.val();
+    const queryCuisine = $(event.currentTarget).find('#cuisine');
+    const queryC = queryCuisine.val();
     // clear out the input
     queryTarget.val("");
     getAddress(query, displayGoogleLatitude);
